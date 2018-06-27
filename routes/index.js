d835c73213158c2e2caf38587410eb8909a2741f
Geoff Wall <gwall1@godaddy.com>
2018-06-27 16:55:00 -0700

    [wip dist] Started on status routes.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-
+const asynHandler = require('express-async-handler');
 /**
  * Routes initializer
  *
@@ -9,7 +9,32 @@
  */
 module.exports = function routes(app, options, next) {
   app.perform('actions', function (done) {
-    // routes go here
+
+    const statusRouteHandler = function statusRouteHandler(type) {
+      const find = type === 'StatusEvent' ? 'findAll' : 'findOne';
+
+      return async function (req, res) {
+        const { pkg, env, version } = req.params;
+
+        if(pkg && env && version) {
+          const status = await app.models[type][find]({ pkg, env, version });
+
+          return res.json(status);
+        }
+  
+        if(pkg && env) {
+          const head = await app.models.StatusHead.findOne({ pkg, env });
+          const status = await app.models[type][find]({ pkg, env, version: head.version });
+
+          return res.json(status);
+        }
+
+        res.status(400).json(new Error(`Bad path. ${!pkg && 'Missing package. '} ${!env && 'Missing environment.'}`));
+      };
+    };
+    
+    app.routes.get('/status/:pkg/:env/:version', asynHandler( statusRouteHandler('Status') ));
+    app.routes.get('/status-events/:pkg/:env/:version', asynHandler( statusRouteHandler('StatusEvent') ));
 
     done();
   });
