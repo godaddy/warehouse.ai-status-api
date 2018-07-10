acbf40616a73c085443b1e58f54753e0c6c3272e
Jarrett Cruger <jcrugzz@gmail.com>
2018-07-10 14:04:57 -0400

    [test] another simple test

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 const index = require('..');
+const assume = require('assume');
 
 describe('index', function () {
   let app;
@@ -13,6 +14,11 @@ describe('index', function () {
   after(function (done) {
     if (app) return app.close(done);
     done();
+  });
+
+  it('should correctly listen on a port', function () {
+    const port = app.servers.http.address().port;
+    assume(port).is.a('number');
   });
 
   it('it should respond with 404 on root');
