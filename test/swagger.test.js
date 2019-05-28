77bde73eb6577c276f2faef10c45c9846e47915b
Jarrett Cruger <jcrugzz@gmail.com>
2019-05-28 13:08:02 -0400

    [fix] generate doc

--- test/swagger.test.js
+++ test/swagger.test.js
@@ -1,8 +1,10 @@
 const assume = require('assume');
+const { execSync } = require('child_process');
 
 describe('Swagger docs', function () {
 
   it('includes swagger documentation', function () {
+    execSync('npm run swagger');
     const docs = require('../swagger/documentation.json');
     assume(docs.openapi).equal('3.0.2');
     assume(docs.paths).not.is.falsey();
